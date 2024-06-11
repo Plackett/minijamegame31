57d3f256d13658e5c465bc2c36a989365d655e7b
Plackett <johnnicholasdimitri@gmail.com>
2024-06-11 18:34:09 -0400

    oob glitch patched

--- Assets/ConstraintedMovement.cs
+++ Assets/ConstraintedMovement.cs
@@ -6,6 +6,9 @@ public class ConstraintedMovement : MonoBehaviour
 {
     public bool movementActive = false;
     private bool playing = false;
+    Vector3 moveAxis;
+    public float speed = 1;
+    public float CamSpeed = 1;
 
     private float yaw = 0.0f;
     private float pitch = 0.0f;
@@ -22,39 +25,49 @@ public class ConstraintedMovement : MonoBehaviour
         {
             Cursor.lockState = CursorLockMode.Locked;
             Cursor.visible = true;
-            if(Mathf.Abs(Input.GetAxis("Horizontal")) > 0.1f)
+            moveAxis = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+            moveAxis = Vector3.ClampMagnitude(moveAxis, 1);
+            if (Mathf.Abs(Input.GetAxis("Horizontal")) > 0.1f)
             {
                 playing = true;
-                transform.Translate(Vector3.right * 4 * Time.deltaTime * Input.GetAxis("Horizontal"));
             }
             if (Mathf.Abs(Input.GetAxis("Vertical")) > 0.1f)
             {
                 playing = true;
-                transform.Translate(Vector3.forward * 4 * Time.deltaTime * Input.GetAxis("Vertical"));
             }
             if(Mathf.Abs(Input.GetAxis("Horizontal")) < 0.1f && Mathf.Abs(Input.GetAxis("Vertical")) < 0.1f)
             {
                 playing = false;
-                this.GetComponent<AudioSource>().Pause();
+                this.GetComponent<AudioSource>().Stop();
             }
             else if(playing == true)
             {
-                this.GetComponent<AudioSource>().UnPause();
+                this.GetComponent<AudioSource>().Play();
             }
+            
             if (Mathf.Abs(Input.GetAxis("Mouse X")) > 0.1f)
             {
-                yaw += 2 * Input.GetAxis("Mouse X");
+                yaw += CamSpeed * Input.GetAxis("Mouse X");
             }
             if (Mathf.Abs(Input.GetAxis("Mouse Y")) > 0.1f)
             {
-                pitch -= 2 * Input.GetAxis("Mouse Y");
+                pitch -= CamSpeed * Input.GetAxis("Mouse Y");
             }
             if (pitch > 90.0f) { pitch = 90.0f; }
             if (pitch < -90.0f) { pitch = -90.0f; }
-            transform.eulerAngles = new Vector3(0.0f, yaw, 0.0f);
-            transform.GetChild(0).eulerAngles = new Vector3(pitch,yaw,0.0f);
-            transform.GetChild(0).position = new Vector3(0, 1, 0) + transform.position;
         }
         else { this.GetComponent<AudioSource>().Stop(); }
+    }
+
+    private void FixedUpdate()
+    {
+        if(movementActive == true)
+        {
+            this.GetComponent<Rigidbody>().rotation = Quaternion.Euler(new Vector3(0f,yaw,0f));
+            transform.GetChild(0).GetComponent<Rigidbody>().rotation = Quaternion.Euler(new Vector3(pitch, yaw, 0.0f));
+            this.GetComponent<Rigidbody>().MovePosition(this.GetComponent<Rigidbody>().position + transform.forward * moveAxis.z * speed * Time.deltaTime);
+            this.GetComponent<Rigidbody>().MovePosition(this.GetComponent<Rigidbody>().position + transform.right * moveAxis.x * speed * Time.deltaTime);
+            transform.GetChild(0).position = new Vector3(0, 1, 0) + transform.position;
+        }
     }
 }
